7b5acbf6306c1c4b8cc6f727f29d904e1cccf1f1
devops <devops@dev.ops>
2025-01-02 09:46:29 +0900

    file arranged

--- state/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket = "terraform-ssg-rlwjd"
-   key = "workspaces-example/terraform.tfstate"
-   region = "us-east-2"
-   dynamodb_table = "terraform-locks"
-   encrypt = true
- }
-}
